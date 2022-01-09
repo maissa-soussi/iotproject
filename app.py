76f82c29912f7a02ebbb25caed942236385587ac
farouk kassebi <farouk.kassebi@ensi-uma.tn>
2022-01-09 12:13:36 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Blueprint , jsonify , request,Flask
+from flask_cors import CORS,cross_origin
+import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as subscribe
+
+#driver = GraphDatabase.driver( "bolt://localhost:7687",auth=basic_auth("neo4j", "123456"))
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/subscriber',methods=['GET'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def getTemp():
+    topics = ['TEMPERATURE']
+    msg = subscribe.simple(topics, hostname="broker.hivemq.com", retained=False, msg_count=1)
+    temp = float(msg.payload)
+    return jsonify([{'temperature': temp}])
+
+
+@app.route('/temperatures',methods=['GET'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def getTemps():
+    topics = ['TEMPERATURE']
+    msg = subscribe.simple(topics, hostname="broker.hivemq.com", retained=False, msg_count=10)
+    json = []
+    for a in msg:
+        temp = float(a.payload)
+        dict = {'temperature': temp}
+        json.append(dict)
+    return jsonify(json)
---
+++ publisher.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+from random import randrange, uniform
+import time
+
+#hivemq broker connection
+mqttBroker = "broker.hivemq.com"
+client = mqtt.Client("Temperature_Outside")
+client.connect(mqttBroker)
+
+#publishing temperature
+while True:
+    randNumber = uniform(0.0, 50.0)
+    # topic: "TEMPERATURE"
+    client.publish("TEMPERATURE", randNumber)
+    print("Temperature outside : " + str(randNumber))
+    time.sleep(1)
